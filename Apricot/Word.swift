a0149c451060bb4a783309f6a7f2774ad32a4092
Masaaki Kawata <kawatan@gmail.com>
2024-12-29 15:30:24 +0900

    Update Word.swift

--- Apricot/Word.swift
+++ Apricot/Word.swift
@@ -10,10 +10,16 @@ import Foundation
 public struct Word: Identifiable, Hashable, Codable {
     public var id: UUID? = UUID()
     public var name: String
-    public var attributes: [String] = []
+    public var attributes: [String]?
     
     public init(name: String) {
         self.name = name
+        self.attributes = []
+    }
+    
+    public init(name: String, attributes: [String]?) {
+        self.name = name
+        self.attributes = attributes
     }
     
     enum CodingKeys: String, CodingKey {
