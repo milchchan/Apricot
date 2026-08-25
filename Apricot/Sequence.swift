d3b81533071f4f1023e912e6156fc5828f2126c6
Masaaki Kawata <kawatan@gmail.com>
2026-08-25 12:30:57 +0900

    Update Sequence.swift

--- Apricot/Sequence.swift
+++ Apricot/Sequence.swift
@@ -7,40 +7,52 @@
 
 import Foundation
 
-public class Sequence: Collection {
+public struct Sequence: Collection, Identifiable, Sendable {
+    public indirect enum Step: Sendable {
+        case message(Message)
+        case synthesis(Message, String)
+        case animations([Animation])
+        case sound(Sound)
+        case audio(Data)
+        case sequence(Sequence)
+        case completion
+    }
+
+    public let id: UUID
     public var name: String?
     public var state: String?
-    private var segments: [Any?] = []
+    private var steps: [Step] = []
     public var startIndex: Int {
-        return self.segments.startIndex
+        return self.steps.startIndex
     }
     public var endIndex: Int {
-        return self.segments.endIndex
+        return self.steps.endIndex
     }
     
     public init(name: String?, state: String? = nil) {
+        self.id = UUID()
         self.name = name
         self.state = state
     }
     
-    public subscript(position: Int) -> Any? {
-        self.segments[position]
+    public subscript(position: Int) -> Step {
+        self.steps[position]
     }
     
     public func index(after i: Int) -> Int {
-        return self.segments.index(after: i)
+        return self.steps.index(after: i)
     }
     
-    public func append(_ newElement: Any?) {
-        self.segments.append(newElement)
+    public mutating func append(_ newElement: Step) {
+        self.steps.append(newElement)
     }
     
     @discardableResult
-    public func remove(at index: Int) -> Element {
-        return self.segments.remove(at: index)
+    public mutating func remove(at index: Int) -> Element {
+        return self.steps.remove(at: index)
     }
     
-    public func removeAll(keepingCapacity keepCapacity: Bool = false) {
-        self.segments.removeAll(keepingCapacity: keepCapacity)
+    public mutating func removeAll(keepingCapacity keepCapacity: Bool = false) {
+        self.steps.removeAll(keepingCapacity: keepCapacity)
     }
 }
